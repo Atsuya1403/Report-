0496241a07a6ddd5eb8d05bfbffafc2135f21c6d
Hijikata  Atsuya <e195749.u-ryukyu.ac.jp>
2019-12-09 03:56:53 +0900

    step3

--- src/hizikata/Main.java
+++ src/hizikata/Main.java
@@ -2,15 +2,21 @@ package hizikata;
 
 class main {
     public static void main(String[] args){
+        //String str;
+        //str = null;
+        //try {
+            //System.out.println(str.length());
+        //}catch (Exception e){
+            //System.out.println("例外が発生しました");
+            //System.out.println(e.getMessage());
+        //}
         String str;
-        str = null;
-        try {
-            System.out.println(str.length());
-        }catch (Exception e){
-            System.out.println("例外が発生しました");
-            System.out.println(e.getMessage());
-        }
+        str = "百二十三";
+        int i = Integer.parseInt(str);
+        System.out.println(i);
 
     }
 }
+
+
 
